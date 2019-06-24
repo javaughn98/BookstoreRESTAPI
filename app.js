59d7e692b2b18b8420a34e36d1701bc7d287eb7c
Javaughn Burke <javaughnburke@Javaughns-MacBook-Air.local>
2019-06-24 16:42:07 -0400

    complete bookstore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');   
 const Genre = require('./models/genre.js');
 const Book = require('./models/book.js');
 
